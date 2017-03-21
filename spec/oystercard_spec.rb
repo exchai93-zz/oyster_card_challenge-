d870fd83f1bebaca5981171566d2e577b95e86de
Emily Chai <echai93@gmail.com>
2017-03-21 10:42:28 +0000

    User Story 1: Added balance method to Oystercard class and passed tests

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,15 @@
+require 'oystercard'
+
 describe Oystercard do
-  
+  subject(:card) {described_class.new}
+
+  describe '#balance' do
+    it { is_expected.to respond_to :balance }
+
+    it "initialized with balance of 0" do
+      expect(card.balance).to eq 0
+    end
+  end
+
+
 end
--- spec/user_story_spec.rb
+++ spec/user_story_spec.rb
@@ -1,3 +1,12 @@
+
 describe "User Stories" do
-  
+
+  let(:card) {Oystercard.new}
+  # In order to use public transport
+  # As a customer
+  # I want money on my card
+  it "In order to use public transport, a card stores money" do
+    expect{ card.balance }.not_to raise_error
+  end
+
 end
